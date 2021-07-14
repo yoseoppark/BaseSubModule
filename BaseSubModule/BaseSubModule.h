b8cf99f8484bc1c283e219213dc12f02170c822d
yoseop park <yoseop.park@hyundai.com>
2021-07-14 11:29:49 +0900

    Initial Commit

---
+++ BaseSubModule/BaseSubModule.h
@@ -0,0 +1,18 @@
+//
+//  BaseSubModule.h
+//  BaseSubModule
+//
+//  Created by YoseopPark on 2021/07/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseSubModule.
+FOUNDATION_EXPORT double BaseSubModuleVersionNumber;
+
+//! Project version string for BaseSubModule.
+FOUNDATION_EXPORT const unsigned char BaseSubModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseSubModule/PublicHeader.h>
+
+
