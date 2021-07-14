b8334db4b26608545ce0f726f3a466ef29e53580
yoseop park <yoseop.park@hyundai.com>
2021-07-14 11:55:31 +0900

    파일 추가.

---
+++ BaseSubModule/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  BaseSubModule
+//
+//  Created by YoseopPark on 2021/07/14.
+//
+
+import Foundation
+
+open class BaseViewController: UIViewController {
+    func test() {
+        
+    }
+}
+
+open class GalleryViewController: UIViewController {
+    func foo() {
+        
+    }
+    
+    func bar() {
+        
+    }
+}
